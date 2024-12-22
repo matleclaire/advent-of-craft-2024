11f2b021f2db57aaba329a8126c31dff1bc30eb4
matleclaire <mat.leclaire@gmail.com>
2024-12-22 16:39:54 +0100

    day 04 - add test with mockito

--- exercise/java/day04/src/test/java/routine/RoutineTests.java
+++ exercise/java/day04/src/test/java/routine/RoutineTests.java
@@ -2,10 +2,24 @@ package routine;
 
 import org.junit.jupiter.api.Test;
 
+import static org.mockito.Mockito.*;
+
 class RoutineTests {
     @Test
     void startRoutineWithMockito() {
-        // Write a test using Mockito
+        var mockEmailService = mock(EmailService.class);
+        var mockSchedueService = mock(ScheduleService.class);
+        var schedule = new Schedule();
+        when(mockSchedueService.todaySchedule()).thenReturn(schedule);
+        var mockReindeerFeeder = mock(ReindeerFeeder.class);
+        var routine = new Routine(mockEmailService, mockSchedueService, mockReindeerFeeder);
+
+        routine.start();
+
+        verify(mockSchedueService).organizeMyDay(schedule);
+        verify(mockReindeerFeeder).feedReindeers();
+        verify(mockEmailService).readNewEmails();
+        verify(mockSchedueService).continueDay();
     }
 
     @Test
